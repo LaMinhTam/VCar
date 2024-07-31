816fae402b388703e5be5ceb52f3b22cfc76485d
Your Name~clear <vodinhthong17112003@gmail.com>
2024-07-31 17:12:12 +0700

    initialize VCarMobile

---
+++ VCarMobile/App.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text className='text-red-500'>App</Text>
+    </View>
+  )
+}
