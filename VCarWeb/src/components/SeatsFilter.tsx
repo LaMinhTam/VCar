b9950f3a2bc66aa3beb91418ea737c4ce137cdb4
laminhtam <laminhtam5523@gmail.com>
2024-08-20 15:52:14 +0700

    feat: change seat options from 2,4,6,8,10 to 2,5,7,9,10 and use 10+ for options exceeding 9

--- VCarWeb/src/components/SeatsFilter.tsx
+++ VCarWeb/src/components/SeatsFilter.tsx
@@ -11,8 +11,8 @@ const SeatsFilter: React.FC<SeatsFilterProps> = ({ seats, onSeatsChange }) => {
   const { t } = useTranslation();
 
   const handleSeatsChange = (value: number | string) => {
-    if (value === "More") {
-      onSeatsChange(10); // Set value to 10 if "More" is selected
+    if (value === "10+") {
+      onSeatsChange(10); // Set value to 10 if "10+" is selected
     } else {
       if (seats.includes(value as number)) {
         onSeatsChange(value as number); // Uncheck the value if it's already selected
@@ -26,11 +26,11 @@ const SeatsFilter: React.FC<SeatsFilterProps> = ({ seats, onSeatsChange }) => {
     <div className="mb-6">
       <h3 className="text-lg font-semibold mb-2">{t("seats")}</h3>
       <div className="flex flex-col gap-2">
-        {[2, 4, 6, 8, "More"].map((num) => (
+        {[2, 5, 7, 9, "10+"].map((num) => (
           <label key={num} className="flex items-center gap-2">
             <CustomCheckbox
               checked={
-                num === "More"
+                num === "10+"
                   ? seats.includes(10)
                   : seats.includes(num as number)
               }
