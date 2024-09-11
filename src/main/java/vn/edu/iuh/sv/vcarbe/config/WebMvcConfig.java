265b356bd4df309e10206b974142defbc12808ee
laminhtam <laminhtam5523@gmail.com>
2024-09-11 20:15:29 +0700

    feat: update cors on specific port

--- src/main/java/vn/edu/iuh/sv/vcarbe/config/WebMvcConfig.java
+++ src/main/java/vn/edu/iuh/sv/vcarbe/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package vn.edu.iuh.sv.vcarbe.config;
 
-import com.google.api.client.util.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -9,8 +9,18 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class WebMvcConfig implements WebFluxConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
