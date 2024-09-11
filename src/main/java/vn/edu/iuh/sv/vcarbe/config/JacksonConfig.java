9d252616d210045c259130248a7824206404d025
laminhtam <laminhtam5523@gmail.com>
2024-09-11 20:17:08 +0700

    feat: Ensure custom ObjectMapper with snake_case is applied in WebFlux

--- src/main/java/vn/edu/iuh/sv/vcarbe/config/JacksonConfig.java
+++ src/main/java/vn/edu/iuh/sv/vcarbe/config/JacksonConfig.java
@@ -6,9 +6,15 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class JacksonConfig {
+@EnableWebFlux
+public class JacksonConfig implements WebFluxConfigurer {
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -17,5 +23,12 @@ public class JacksonConfig {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // Exclude null values
         objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false); // Exclude null map values
         return objectMapper;
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        ObjectMapper objectMapper = objectMapper();
+        configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
     }
 }
