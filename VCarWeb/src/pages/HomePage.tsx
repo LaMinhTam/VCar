e73f0eef3c59b01a140a8c34918e2d39ae99269a
Your Name~clear <vodinhthong17112003@gmail.com>
2024-10-02 22:39:19 +0700

    feat: setup firebase and firebase cloud messaging

--- VCarWeb/src/pages/HomePage.tsx
+++ VCarWeb/src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
 
+import { useEffect } from "react";
 import Banner from "../modules/home/Banner";
 import CarSession from "../modules/home/CarSession";
 import FilterCar from "../modules/home/FilterCar";
+import { generateToken } from "../config/firebaseConfig";
 
 const HomePage = () => {
+
+  useEffect(() => {
+    async function getToken() {
+      await generateToken();
+    }
+    getToken();
+  }, [])
   return (
     <div>
       <Banner></Banner>
